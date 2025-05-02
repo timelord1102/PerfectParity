ebbde120d2bf12da2bd9b17669da1c928f016d2f
montmaj <timelord1102@gmail.com>
2025-05-02 04:22:49 -0400

    Chicken bugfix

--- src/main/java/com/perfectparity/mixin/ChickenMixin.java
+++ src/main/java/com/perfectparity/mixin/ChickenMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntitySpawnReason;
@@ -77,7 +78,7 @@ public class ChickenMixin extends Animal implements VariantMob {
 
     @Override
     public boolean isFood(ItemStack itemStack) {
-        return false;
+        return itemStack.is(ItemTags.CHICKEN_FOOD);
     }
 
     @Override
